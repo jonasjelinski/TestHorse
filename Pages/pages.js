faffa76410af5bf98f334212efc912c953a25c68
Unknown <jonas.jelinski@stud.uni-regensburg.de>
2018-08-02 11:26:21 +0200

    Kommentiere aus

--- Pages/pages.js
+++ Pages/pages.js
@@ -8,21 +8,66 @@ var Pages = Pages || {};
  */
 Pages = function(){
 	let that = {},
-		domElement,
+		pageDomElement,
 		pageChanger,
 		mainPage;
 
+	/**
+	* @function init
+	* @public
+	* @memberof! Pages.PageChanger  
+	* @instance
+	* @description Initialize this model.
+	*/ 
 	function init(){
-		domElement = document.getElementById("parentPage"),
-		pageChanger = Pages.PageChanger(domElement);
-		pageChanger.init();
-		mainPage = new MainPage();		
+		initPageChanger();
+		initPageVariables();				
 	}
 
+	/**
+	* @function initPageChanger
+	* @private
+	* @memberof! Pages.PageChanger  
+	* @instance
+	* @description Initialize this model.
+	* <p>pageDomElement</p> is the dom-element e.g. a "div" which contains the page. 
+	* <p>pageChanger</p> changes the user interface to the htmlText of the different pages.	
+	*/ 
+	function initPageChanger(){
+		pageDomElement = document.getElementById("parentPage"),
+		pageChanger = Pages.PageChanger(pageDomElement);
+		pageChanger.init();
+	}
+
+	/**
+	* @function initPageVariables
+	* @private
+	* @memberof! Pages.PageChanger  
+	* @instance
+	* @description sets the variables of this modul which contain the moduls of the different pages.
+	*/ 
+	function initPageVariables(){
+		mainPage = new MainPage();
+	}
+
+	/**
+	* @function showLoginPage
+	* @private
+	* @memberof! Pages.PageChanger  
+	* @instance
+	* @description shows the loginPage to the user
+	*/ 
 	function showLoginPage(){
 		pageChanger.switchPage("LOGIN");
 	}
 
+	/**
+	* @function showMainPage
+	* @private
+	* @memberof! Pages.PageChanger  
+	* @instance
+	* @description shows the mainPage to the user
+	*/ 
 	function showMainPage(){
 		pageChanger.switchPage("MAIN");
 		mainPage.init();
