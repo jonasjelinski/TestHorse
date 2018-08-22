76a4d5fe82d40463b168b17c40b8847f8a261557
Unknown <jonas.jelinski@stud.uni-regensburg.de>
2018-08-22 11:50:40 +0200

    Kommentiere Code

--- HorseProfileSaver/horsePorfileSaverDBRequester.js
+++ HorseProfileSaver/horsePorfileSaverDBRequester.js
@@ -1,31 +1,79 @@
 var HorseProfileSaver = HorseProfileSaver || {};
 
-HorseProfileSaver.DBRequester = function(userID){
+/** 
+ * namespace HorseProfileSaver
+ * @memberof! DBRequester
+ * @param {string}, userID id of the user
+ * @description <code>HorseProfilePage.DBRequester</code> 
+ * is used to save a horse into the database.
+ */
+ HorseProfileSaver.DBRequester = function(userID){
 	let that = {},
 		isNewHorse,
 		newHorse;
 
+	/**
+	* @function init
+	* @public
+	* @memberof!  HorseProfileSaver.DBRequester 
+	* @instance
+	* @param {object}, horse, horse which will be saved into the database
+	* @description Initialize this model
+	*/ 
 	function init(horse) {
 		newHorse = horse;
 		addUserId();
 		initRequester();
 	}
 
+	/**
+	* @function userID
+	* @private
+	* @memberof!  HorseProfileSaver.DBRequester 
+	* @instance
+	* @description adds the userID to the horse, because the database
+	* needs to know the userID for saving the horse
+	*/ 
 	function addUserId(){
 		newHorse.userID = userID;
 	}
 
+	/**
+	* @function initRequester
+	* @private
+	* @memberof!  HorseProfileSaver.DBRequester 
+	* @instance
+	* @description inits the database requester
+	*/ 
 	function initRequester() {
 		requester = new DatabaseClientInterface();
 		requester.addEventListener("onResult", handleResult);
 		requester.init();
 	}
 
+	/**
+	* @function handleResult
+	* @private
+	* @memberof! HorseProfileSaver.DBRequester 
+	* @instance
+	* @param {event} event, contains the result of the db request
+	* @description handles the result of the db request
+	*/
 	function handleResult(event){
 		let result = event.details.result;
 		console.log("horse saver result", result);
 	}
 
+	/**
+	* @function saveHorseIntoDB
+	* @public
+	* @memberof! HorseProfileSaver.DBRequester 
+	* @instance
+	* @description saves the horse into the database.
+	* if it is an old horse which is allready in the database
+	* another request has to be called to the databse,
+	* therefore this function distincts between this two cases.
+	*/
 	function saveHorseIntoDB(){
 		let hadCorrectParameter = {};
 		if(isNewHorse){
@@ -39,6 +87,14 @@ HorseProfileSaver.DBRequester = function(userID){
 		handleParameterFeedBack(hadCorrectParameter);
 	}
 
+	/**
+	* @function handleParameterFeedBack
+	* @private
+	* @memberof! HorseProfileSaver.DBRequester 
+	* @instance
+	* @param {bool} hadCorrectParameter
+	* @description gives a feedback if the object given to the requester had the correct properties
+	*/
 	function handleParameterFeedBack(hadCorrectParameter){
 		if(hadCorrectParameter){
 			console.log("hadCorrectParameter")
@@ -48,10 +104,27 @@ HorseProfileSaver.DBRequester = function(userID){
 		}
 	}
 
+	/**
+	* @function setUpdateHorse
+	* @public
+	* @memberof! HorseProfileSaver.DBRequester 
+	* @instance
+	* @description sets <code>isNewHorse</code> false, so this modul
+	* know it has to update an old horse and not save a new horse in the database
+	*/
 	function setUpdateHorse(){
 		isNewHorse = false;
 	}
 
+	
+	/**
+	* @function setNewHorse
+	* @public
+	* @memberof! HorseProfileSaver.DBRequester 
+	* @instance
+	* @description sets <code>isNewHorse</code> true, so this modul
+	* know it has to save a new horse in the database
+	*/
 	function setNewHorse(){
 		isNewHorse = true;
 	}
--- HorseProfileSaver/horseProfileSaver.js
+++ HorseProfileSaver/horseProfileSaver.js
@@ -1,5 +1,14 @@
 var HorseProfileSaver = HorseProfileSaver || {};
 
+/** 
+ * namespace HorseProfileSaver
+ * @memberof! HorseProfileSaver
+ * @param {string}, userID id of the user
+ * @description <code>HorseProfileSaver</code> shows the profile of a horse to the user
+ * and allows him to save it into the database. It is used after the user either created
+ * a new horse with <code>HorseCreatorPage</code> or updated an old horse with <code>HorseProfileChanger</code>.
+ * The use can cancel the saving, if he wants.
+ */
 HorseProfileSaver = function(userID){
 	"user strict";
 	const 
@@ -12,33 +21,82 @@ HorseProfileSaver = function(userID){
 		dbInterface,
 		attributes;
 
+
+	/**
+	* @function init
+	* @public
+	* @memberof! HorseProfileSaver
+	* @instance
+	* @param {newAttributes}, newAttributes, attributes of the horse
+	* @description Initialize this model
+	*/ 
 	function init(newAttributes){	
 		attributes = newAttributes;
 		initPofil();
-		initModel();
+		initDBRequester();
 		addListeners();			
 	}
 
+	/**
+	* @function initPofil
+	* @private
+	* @memberof! HorseProfileSaver
+	* @instance
+	* @description Initialize <code>profil</code> an instance of Profil
+	*/ 
 	function initPofil(){
 		profil = new Profil(PAGE_ID, TEMPLATE_ID, HorseProfileSaver.ViewControll);
 		profil.init(attributes);		
 	}
 
-	function initModel(){
+	/**
+	* @function initPofil
+	* @private
+	* @memberof! HorseProfileSaver
+	* @initModel
+	* @description Initialize the db requester <code>dbInterface</code>
+	*/ 
+	function initDBRequester(){
 		dbInterface = new HorseProfileSaver.DBRequester(userID);
 		dbInterface.init(attributes);
 	}
 
+	/**
+	* @function initPofil
+	* @private
+	* @memberof! HorseProfileSaver  
+	* @instance
+	* @description adds eventListener to the profile, if the user wants to change,
+	* save or delte the horse.
+	*/
 	function addListeners(){
 		profil.addEventListener("onChangeProfile", handleChangeProfile);
 		profil.addEventListener("onProfileOkay", handleOkayProfile);
 		profil.addEventListener("onDeleteProfile", handleDeleteProfile);	
 	}
 
+	/**
+	* @function handleChangeProfile
+	* @private
+	* @memberof! HorseProfileSaver  
+	* @instance
+	* @description sends an event of type "onChangeHorseProfile"
+	* and the attributes of the horse so <code>HorseProfileChanger</code>.
+	* can change the horse if the user is unsatisfied with the horse,
+	* which he wanted to save into the database
+	*/
 	function handleChangeProfile(){
 		sendEvent("onChangeHorseProfile", attributes);
 	}
 
+	/**
+	* @function sendEvent
+	* @private
+	* @memberof! HorseProfileSaver
+	* @instance
+	* @param {string} type,type of event
+	* @description sends event of type "type" and the attributes of the horse
+	*/
 	function sendEvent(type, attributes){
 		let event = new Event(type);
 		event.details = {};
@@ -46,19 +104,49 @@ HorseProfileSaver = function(userID){
 		that.dispatchEvent(event);
 	}
 
+	/**
+	* @function handleOkayProfile
+	* @private
+	* @memberof! HorseProfileSaver
+	* @instance
+	* @description saves the horse into the database and sends the event "onSaveHorseProfile"
+	*/
 	function handleOkayProfile(){
 		dbInterface.saveHorseIntoDB();
 		sendEvent("onSaveHorseProfile");
 	}
 
+	/**
+	* @function handleDeleteProfile
+	* @private
+	* @memberof! HorseProfileSaver
+	* @instance
+	* @description sends the event "onDeleteNewHorseProfile" and cancels the saving process
+	*/
 	function handleDeleteProfile(){
 		sendEvent("onDeleteNewHorseProfile", "");
 	}
 
+	/**
+	* @function setDBInterfaceToUpdate
+	* @private
+	* @memberof! HorseProfileSaver
+	* @instance
+	* @description sets the dbInterface to update, so the dbInterface knows
+	* that it has to update and old horse and not saving a new one
+	*/
 	function setDBInterfaceToUpdate(){
 		dbInterface.setUpdateHorse();
 	}
 
+	/**
+	* @function setDBInterfaceToNew
+	* @private
+	* @memberof! HorseProfileSaver
+	* @instance
+	* @description sets the dbInterface to new, so the dbInterface knows
+	* it has to save a new one horse  and not update an old one
+	*/
 	function setDBInterfaceToNew(){
 		dbInterface.setNewHorse();
 	}
